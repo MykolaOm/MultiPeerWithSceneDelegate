ccc08fa387d1d06c90789f2eb10e081afb6cb88d
Mykola Omelianov <mykola.omelianov@smartexe.com>
2022-06-21 21:22:55 +0300

    Initial Commit

---
+++ pee2sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pee2sample
+//
+//  Created by Nikolas Omelianov on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
